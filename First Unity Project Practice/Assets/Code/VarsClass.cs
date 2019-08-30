e742eb91d9b19acbb3908ca827fbfa5519a366d2
Jaycie <10757533@my.uvu.edu>
2019-08-30 08:13:05 -0600

    Added Print Words from class

---
+++ First Unity Project Practice/Assets/Code/VarsClass.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+public class VarsClass : MonoBehaviour 
+{
+	private void Start () 
+	{
+		print(message: "Annyeong");
+		print(message: "it");
+		print(message: "leaves me");
+		print(message: "feelin");
+		print(message: "Seasick");
+	}
+
+}
