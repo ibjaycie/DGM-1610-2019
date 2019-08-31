10a525cffaa17eee30b4c832708c871631a78ef5
Jaycie <10757533@my.uvu.edu>
2019-08-31 13:14:16 -0600

    Added Vertical Movement on cube

--- First Unity Project Practice/Assets/Code/CubeMove.cs
+++ First Unity Project Practice/Assets/Code/CubeMove.cs
@@ -8,5 +8,8 @@ public class CubeMove : MonoBehaviour
 	{
 		Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"),0.0f, 0.0f);
 		transform.position = transform.position + horizontal * Time.deltaTime;
+		Vector3 vertical = new Vector3(0.0f, Input.GetAxis("Vertical"), 0.0f);
+		transform.position = transform.position + vertical * Time.deltaTime;
 	}
+	
 }
