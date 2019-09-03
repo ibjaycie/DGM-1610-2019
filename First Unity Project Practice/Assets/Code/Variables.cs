04e3671947f009d52133f30a8c4fb6b562e15155
Jaycie <10757533@my.uvu.edu>
2019-09-03 14:59:06 -0600

    Added Variables in Class

---
+++ First Unity Project Practice/Assets/Code/Variables.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+	public float floatValue = 5f;
+	public int intValue;
+	public string stringValue = "Chim";
+	public int firePower;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		print("Just passing through");
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
