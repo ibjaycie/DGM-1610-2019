10592b937bc72122b2450e1344d6655c068e651f
Jaycie <10757533@my.uvu.edu>
2019-09-05 08:30:21 -0600

    Added on Mouse Enter and Exit ON MY OWN WOOT WOOT

--- First Unity Project Practice/Assets/Code/PlayerOneEvent.cs
+++ First Unity Project Practice/Assets/Code/PlayerOneEvent.cs
@@ -14,12 +14,29 @@ public class PlayerOneEvent : MonoBehaviour
 		//What i'm trying to do here is when the cube hits the bomb
 		//the cube changes color. that's the intended outcome.
 		Event.Invoke();
-		//GetComponent<Renderer>().material.color = Color.white;
+		//if (other.gameObject.activeSelf) not working. 
+		//{
+		//	GetComponent<Renderer>().material.color = Color.white;
+		//}
 		//Above isn't doing anything so commenting it out for now. 
-		
+		print("change to white please");
+
 
 	}
+	//I initially thought the below script would make the box disappear when i hit enter
+	//turns out its been the Mouse Enters the object. haha i'm an idiot i should have read it.
+	//But hey it works!
+	private void OnMouseEnter()
+	{
+		Event.Invoke();
+		print("I just hit enter");
+	}
 
+	private void OnMouseExit()
+	{
+		Event.Invoke();
+		print("That was a close one");
+	}
 
 	// Use this for initialization
 	void Start () {
