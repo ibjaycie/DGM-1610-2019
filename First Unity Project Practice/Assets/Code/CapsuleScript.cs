3916c196c156554a5c84b6c21dd9f77b2a18dda6
Jaycie <10757533@my.uvu.edu>
2019-09-09 22:04:03 -0600

    Added Collison. Trying to figure out how debug.log and print are different

---
+++ First Unity Project Practice/Assets/Code/CapsuleScript.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CapsuleScript : MonoBehaviour
+{
+	public UnityEvent Event;
+
+	private void OnCollisionEnter(Collision other)
+	{
+		Debug.Log("oops");
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
