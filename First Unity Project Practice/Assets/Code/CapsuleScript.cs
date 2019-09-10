6b4d65e47ec69453dac5aa78c4f693cb64951712
Jaycie <10757533@my.uvu.edu>
2019-09-09 22:15:43 -0600

    Added collision exit and destory still trying

--- First Unity Project Practice/Assets/Code/CapsuleScript.cs
+++ First Unity Project Practice/Assets/Code/CapsuleScript.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using Object = UnityEngine.Object;
 
 public class CapsuleScript : MonoBehaviour
 {
@@ -12,6 +13,16 @@ public class CapsuleScript : MonoBehaviour
 	{
 		Debug.Log("oops");
 	}
+
+	private void OnCollisionExit(Collision other)
+	{
+		print("Did that do anything?");
+		Destroy(gameObject); //It doesn't destroy everything and Idk why. 
+		//I tried a bunch of things. Without a tutorial. 
+	}
+
+	
+	
 
 	// Use this for initialization
 	void Start () {
