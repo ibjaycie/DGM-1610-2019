92e1de88f6a5066e724eb9b11c62d42c25d57ed6
Jaycie <10757533@my.uvu.edu>
2019-09-11 23:15:26 -0600

    Added Same Script from class
    Just reviewing how it was done so I can try and figure things out on my own

---
+++ First Unity Project Practice/Assets/Code/In Class/Variables.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Variables : MonoBehaviour
+{
+	public float floatValue = 5f;
+	public int intValue;
+	public string stringValue = "Chim";
+	public int firePower;
+	//Added in class after commit
+	public UnityEvent Event;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Event.Invoke();
+		print("Just passing Through");
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
---
+++ First Unity Project Practice/Assets/Code/In Class/VarsClass.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+public class VarsClass : MonoBehaviour 
+{
+	private void Start () 
+	{
+		print(message: "Annyeong");
+		print(message: "it");
+		print(message: "leaves me");
+		print(message: "feelin");
+		print(message: "Seasick");
+		print("she. . . ");
+		print("looks like a");
+		print("parrot");
+		print("come fly with me");
+		print("not exactly sure what else to do");
+	}
+
+}
