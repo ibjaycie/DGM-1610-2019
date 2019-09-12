32ba0901c4b26beb0a320ea0a4332a9dac9b5a72
Jaycie <10757533@my.uvu.edu>
2019-09-12 15:53:40 -0600

    Added more Particle Emits in Class yay!

---
+++ First Unity Project Practice/Assets/Code/In Class/ParticlesOnMouseDown.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ParticlesOnMouseDown : MonoBehaviour
+{
+
+	public ParticleSystem explodeOnMouseDown;
+
+	private void OnMouseDown()
+	{
+		explodeOnMouseDown.Emit(100); 
+	}
+
+	void Start () 
+	{
+		
+	}
+	
+	
+}
