d817b5f199efca9f20c4fbd9a3e98cc30baf0edd
Jaycie <10757533@my.uvu.edu>
2019-09-12 15:21:48 -0600

    Added Cat and Dog Events Classes

---
+++ First Unity Project Practice/Assets/Code/In Class/Dog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Dog : MonoBehaviour
+{
+	public UnityEvent doStuff; 
+	private void OnMouseDown()
+	{
+		doStuff.Invoke();
+	}
+}
--- First Unity Project Practice/Assets/Code/Player Scripts/BulletTriggerEvent.cs
+++ First Unity Project Practice/Assets/Code/Player Scripts/BulletTriggerEvent.cs
@@ -34,7 +34,7 @@ public class BulletTriggerEvent : MonoBehaviour
 		//{
 			
 		//}
-	}
+	
 	
 
 }
