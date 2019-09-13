f4fcbd99c4aa72814ac9f52f5f1b91d4904c8453
Jaycie <10757533@my.uvu.edu>
2019-09-13 14:00:46 -0600

    Added mystery button I want the enemies to pop up but need the enemies to be invisible upon start up

---
+++ First Unity Project Practice/Assets/Code/In Class/MysterySwitch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MysterySwitch : MonoBehaviour
+{
+	public GameObject enemies;
+
+	private void OnMouseDown()
+	{
+		print("We have been summoned");
+	}
+}
---
+++ First Unity Project Practice/Assets/Code/In Class/SummonEnemy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SummonEnemy : MonoBehaviour
+{
+	public UnityEvent enemyAppear;
+
+	private void OnMouseExit()
+	{
+		enemyAppear.Invoke();
+	}
+
+	private void Start()
+	{
+		//I want the cube to be invisible when the game starts
+	}
+}
