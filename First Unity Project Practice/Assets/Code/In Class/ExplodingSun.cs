224177227464974e249fb4dbaa63beaac047b8e7
Jaycie <10757533@my.uvu.edu>
2019-09-13 13:34:53 -0600

    Reviewing what we did in class Added Sun, on/off switch, explosion of particles out of the sun

---
+++ First Unity Project Practice/Assets/Code/In Class/ExplodingSun.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ExplodingSun : MonoBehaviour
+{
+	public ParticleSystem particlesExplodeSun;
+
+	private void OnMouseEnter()
+	{
+		particlesExplodeSun.Emit(150);
+	}
+}
---
+++ First Unity Project Practice/Assets/Code/In Class/LightOnOff.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LightOnOff : MonoBehaviour
+{
+	public UnityEvent lightSwitch;
+
+	private void OnMouseDown()
+	{
+		lightSwitch.Invoke();
+	}
+}
