98dfbb916a74a1eb09810a6e307bd0b4be767ebe
Jaycie <10757533@my.uvu.edu>
2019-09-14 13:43:30 -0600

    Added Particle Systems to each enemy Made it so when the enemies are clicked on they explode in particles as well as a enemy health bar that decreases by 5 when killed.

---
+++ First Unity Project Practice/Assets/Code/Player Scripts/ExplodingEnemies.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ExplodingEnemies : MonoBehaviour
+{
+	public ParticleSystem particlesExplodeEnemies;
+
+	private void OnMouseDown()
+	{
+		particlesExplodeEnemies.Emit(20);
+	}
+}
