2f3aa381e0c9eb16b81c27eb7a72184310fade25
Jaycie <10757533@my.uvu.edu>
2019-09-17 15:12:08 -0600

    Added movement
    Done in class

---
+++ First Unity Project Practice/Assets/Code/In Class/movement.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+	public Vector3 position;
+	public CharacterController controller;
+	void Update () 
+	{
+		//transform.Translate(position*Time.deltaTime);
+		controller.Move(position*Time.deltaTime);
+
+	}
+}
