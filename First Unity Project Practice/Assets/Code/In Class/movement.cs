78a5e558f70956243295545bbf55fc956df545a1
Jaycie <10757533@my.uvu.edu>
2019-09-17 15:45:50 -0600

    Added Jump in class Made it so the character can jump in class.

--- First Unity Project Practice/Assets/Code/In Class/movement.cs
+++ First Unity Project Practice/Assets/Code/In Class/movement.cs
@@ -1,13 +1,33 @@
 ﻿
+using System.Threading;
 using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-	public Vector3 position;
+	public float speed = 1f;
+	public float gravity = -3f;
+	public float jumpSpeed = 10f;
+	
+	private Vector3 position;
 	public CharacterController controller;
 	void Update () 
 	{
 		//transform.Translate(position*Time.deltaTime);
+		position.x = speed*Input.GetAxis("Horizontal");
+		if (!controller.isGrounded)
+		{
+			position.y -= gravity;
+		}
+		else
+		{
+			position.y = 0f;
+		}
+		if (Input.GetButtonDown("Jump"))
+		{
+			position.y = jumpSpeed;
+		}
+		
+		
 		controller.Move(position*Time.deltaTime);
 
 	}
