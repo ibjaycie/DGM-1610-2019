0c982dc450deb44186ff6d491cdc85206681778b
Jaycie <10757533@my.uvu.edu>
2019-09-20 22:28:31 -0600

    Added first stages of power up working on power up double jump if I can do it on my own without a tutorial

---
+++ First Unity Project Practice/Assets/Code/Player Scripts/OnTriggerPowerUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class OnTriggerPowerUp : MonoBehaviour
+{
+	public UnityEvent triggerPowerUpEvent;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		triggerPowerUpEvent.Invoke();
+	}
+}
+//okay I made it this far, an event has been made where the player  triggers it and 
+//pick it up. that's all for today. 
