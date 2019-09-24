472283fdb3b9bb8c807166a3773a8f002ca0550c
Jaycie <10757533@my.uvu.edu>
2019-09-24 15:29:49 -0600

    Practices If Statements in class

---
+++ First Unity Project Practice/Assets/Code/In Class/GetFruit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetFruit : MonoBehaviour
+{
+	public bool appleIsRipe, orangeIsRipe, peachIsRipe;
+	
+	void Start () 
+	{
+		if (appleIsRipe)
+		{
+			print("Go Pick the Apple");
+		}
+		else if (orangeIsRipe)
+		{
+			print("Go Pick the Orange");
+		}
+		else if (peachIsRipe)
+		{
+			print("Go Pick the Peach");
+		}
+		else
+		{
+			print("Nothing is Ripe. Well shoot darn. ");
+		}
+		
+	}
+	
+	//correct way to do this, refer to the double jump script! 
+}
---
+++ First Unity Project Practice/Assets/Code/In Class/IfStatements.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+	public int a = 4;
+	public int b = 5;
+	public int c = 10;
+	public string password = "Mellon";
+	public bool lightsOn = false;
+	void Start () 
+	{
+		if (a + b != c) //as long as c is NOT 10, we are good. 
+		{
+			print(true);
+		}
+		else
+		{
+			print(false);
+		}
+
+		if (password != "OU812") //as long as the result is NOT OU812, we're good.
+		{
+			print(true);
+		}
+
+		if (!lightsOn) //! is checking if it is false. Lights on is False, so the 
+						//Statement itself is true. 
+		{
+			print(true);
+		}
+	}
+	
+	
+}
