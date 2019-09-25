70178e06003172b4d6239ec8d880c101b9766261
Jaycie <10757533@my.uvu.edu>
2019-09-25 13:56:23 -0600

    Figured out how to pick up the object! Was applying the script to the wrong object. Looked at brackeys tutorial to make sure I was doing it right after the fact.

--- First Unity Project Practice/Assets/Code/Player Scripts/OnTriggerPowerUp.cs
+++ First Unity Project Practice/Assets/Code/Player Scripts/OnTriggerPowerUp.cs
@@ -1,31 +1,32 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
-using UnityEngine.Events;
+
 public class OnTriggerPowerUp : MonoBehaviour
 {
-	public UnityEvent triggerPowerUpEvent;
+	public GameObject pickUpPowerUp;
 
 	private void OnTriggerEnter(Collider other)
 	{
+		
 		if (other.CompareTag("Player"))
 		{
 			Pickup();
-			Debug.Log("Power Up Picked Up"); //Doesn't work why?
-			print("Power Up picked Up yay"); //Doesn't work why?!?!?!?
+			
 		}
 		
-		triggerPowerUpEvent.Invoke();
 	
 	}
 
 	private void Pickup()
 	{
-		print("Power Up Picked Up"); //Doesn't Work why?
+		//print("Picked Up Power Up");
+		Destroy(gameObject);
 	}
 
 }
+//IT WORKS! I had too much information and the if statment is acutally referring to the Power up and
+//Need to be applied to the power up not the player because is is asking if the "player" is true pick
+//up the object. Figured that out on my own, good job Jaycie. 
 //9/21/19 Added the pickup function with the help of Brackeys tutoiral, but it's not printing after it's 
 //picked up.. . . .
 //okay I made it this far, an event has been made where the player triggers it and 
