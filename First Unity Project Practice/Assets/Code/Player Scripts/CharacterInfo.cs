6a125afbe893b290e93dbfa4d6ae582794afc343
Jaycie <10757533@my.uvu.edu>
2019-09-27 14:00:35 -0600

    Created a new scriptable obeject for my player still in the proccess of creating more and adding more things to it

---
+++ First Unity Project Practice/Assets/Code/Player Scripts/CharacterInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class CharacterInfo : ScriptableObject
+{
+	public string characterName;
+	public string characterDescription;
+	public int health;
+	public int speed;
+	public Sprite weapon;
+	public Sprite artwork;
+	public Color playerColor = Color.yellow;
+
+	
+
+}
