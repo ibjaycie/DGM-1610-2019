f89b3358f8ee89f3acca26015c6e57efe0cd09b2
Jaycie <10757533@my.uvu.edu>
2019-10-03 15:54:45 -0600

    Added Health bar in Class

---
+++ First Unity Project Practice/Assets/Code/In Class/UiImageController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+[RequireComponent(typeof(Image))]
+public class UiImageController : MonoBehaviour
+{
+	private Image img;
+	
+	void Awake ()
+	{
+		img = GetComponent<Image>();
+	}
+
+	public void UpdateImage(FloatData data) //not naming it health because you can use this for any type of bar to update data
+	{
+		img.fillAmount = data.value;
+	}
+}
