9602ae82d5ca14531677800a3bdae814fdca2f46
Jaycie <10757533@my.uvu.edu>
2019-10-08 15:29:47 -0600

    Added Player Data in Class REVIEW THE NOTES YOU TOOK HERE SO YOU CAN FIX IT AT HOME

---
+++ First Unity Project Practice/Assets/Code/In Class/PlayerData.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+[CreateAssetMenu]
+	class PlayerData : ScriptableObject //allow people to select and use characters with different
+	//attributes, used to configure a prefab. ways we can generate characters on our screen. 
+	{
+		public Sprite sprite; //sprites are 2D assets
+		public Color color;
+		public GameObject prefab; 
+		//we are going to use this object as a selector tool. we are saying that all players
+		//in our game have a sprite, color, and prefab. all the same. Why? because it will be
+		//easy to configure players. 
+		//every time we generate a player object we can select different objects of our player.
+		public void InstancePlayer() //we need to instansiate a prefab adn assign a color to it
+		{
+			var newPlayer = Instantiate(prefab); //it's going to take the prefab and put it on the screen.
+			var newSprite = newPlayer.GetComponentInChildren<SpriteRenderer>();
+			//use get component in children because the sprite renderer is a child of the main object in unity. 
+			newSprite.sprite = sprite;
+			newSprite.color = color; //when we instansiate it tell it find a sprite renderer, once it does
+			//we tell it what color it is for the color we picked. 
+		}
+		
+		//what should happen is when I press play, the player will change to either a red diamond
+		//or a green circle upon the players slection. But my player isn't instantiating. 
+		//it's not appearing for some reason and I need to figure out why. I think it's something in
+		//Unity, not the actual code. 
+		//Go into the player prefab and fix something there, I think that's the issue. 
+	}
