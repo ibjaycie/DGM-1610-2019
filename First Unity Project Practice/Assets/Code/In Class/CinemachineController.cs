7b145a227f42b231a5b37aaa90b0b34a747fe0df
Jaycie <10757533@my.uvu.edu>
2019-10-16 19:06:07 -0600

    Added Cinemachine Controller

---
+++ First Unity Project Practice/Assets/Code/In Class/CinemachineController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+[RequireComponent(typeof(CinemachineVirtualCamera))]
+public class CinemachineController : MonoBehaviour
+{
+	public PlayerData playerData;
+	private CinemachineVirtualCamera virtualCamera;
+
+	private void Awake()
+	{
+		virtualCamera = GetComponent<CinemachineVirtualCamera>();
+		playerData.instanceAction = InstanceHandler;
+		playerData.InstancePlayer();
+	}
+
+	private void InstanceHandler(GameObject obj)
+	{
+		virtualCamera.Follow = obj.transform;
+	}
+}
