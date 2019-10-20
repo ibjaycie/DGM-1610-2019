b463486eb9e0d69800982530ced0e8ed9595e32a
Jaycie <10757533@my.uvu.edu>
2019-10-19 19:36:23 -0600

    Added Character Selector Panel
    It is Instansiating my Player (But not the one I choose)
    How do I instantiate multiple different players. 
    do I need to make different prefabs and canvasas? Or apply it to 
    the indvidual button?

---
+++ BT21 Game/Assets/Scripts/CharacterSelector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelector : MonoBehaviour
+{
+	public GameObject player;
+	public Vector3 spawnPosition = new Vector3(0,1,-7);
+	public Bt21PlayerData[] characters;
+	public GameObject friendPanel;
+
+	public void StartGame(int characterChoice) //int assigns a number to the character it will choose
+	{
+		friendPanel.SetActive(false);
+		var spawnedPlayer = Instantiate(player);
+		Bt21PlayerData selectedCharacter = characters[characterChoice];
+	}
+
+}
---
+++ BT21 Game/Assets/Scripts/Data Scripts/Bt21PlayerData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu (menuName = "Character")]
+public class Bt21PlayerData : ScriptableObject
+{
+	public string friendName;
+	public Sprite characterImage;
+	public int speed = 10;
+	public Color outfitColor = Color.red;
+}
