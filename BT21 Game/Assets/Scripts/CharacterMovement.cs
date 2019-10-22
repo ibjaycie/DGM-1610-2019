431f3c488afcc0771a74187120f5dffd489ead59
Jaycie <10757533@my.uvu.edu>
2019-10-21 23:21:18 -0600

    Added siimple movement script to all characters AND THE CINEMACHINE CAMERA IS FOLLOWING THE INSTANSIATED CHARACTER AND I DIDN"T EVEN HAVE TO APPLY THE SCRIP WE DID IN CLASS. I'm loving this.

---
+++ BT21 Game/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+	public float speed = 1f;
+	public float gravity = -3f;
+
+	private Vector3 position;
+	public CharacterController controller;
+
+	private void Update()
+	{
+		position.x = speed * Input.GetAxis("Horizontal");
+		controller.Move(position * Time.deltaTime);
+	}
+}
