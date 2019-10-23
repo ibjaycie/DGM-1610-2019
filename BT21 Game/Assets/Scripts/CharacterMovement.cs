72897962e3aba35832588e303edfb5cfb896a688
Jaycie <10757533@my.uvu.edu>
2019-10-23 15:57:18 -0600

    Added Double Jump It's not working right now. For somereason gravity isn't being applied. I tried adding a rigidbody but it didn't do anything. I think gravity is the issue beecause the moment i jump it just falls up into endless space.

--- BT21 Game/Assets/Scripts/CharacterMovement.cs
+++ BT21 Game/Assets/Scripts/CharacterMovement.cs
@@ -1,19 +1,42 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
-
+[RequireComponent(typeof(CharacterController))]
 public class CharacterMovement : MonoBehaviour
 {
 	public float speed = 1f;
-	public float gravity = -3f;
+	public float jumpSpeed = 20f;
+	public float gravity = 9.81f;
 
+	private int jumpCount;
+	public int jumpCountMax = 2;
 	private Vector3 position;
 	public CharacterController controller;
+
+
+	private void Start()
+	{
+		controller = GetComponent<CharacterController>();
+	}
 
 	private void Update()
 	{
 		position.x = speed * Input.GetAxis("Horizontal");
+		controller.Move(position * Time.deltaTime);
+		//Now to make the double jump. I made the jumpcountmax 2 BUT I want to make other players capable of more or less
+		//jumps. For instance, Kookie will have ability to jump max 3 times. But I assume I change that in unity
+		//because it is public. 
+		if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
+		{
+			position.y = jumpSpeed;
+			jumpCount++;
+		}
+
+		if (controller.isGrounded)
+		{
+			position.y = 0f;
+			jumpCount = 0; 
+		}
+
 		controller.Move(position * Time.deltaTime);
 	}
 }
