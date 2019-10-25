c25a326573e8ad729fa45b165138559efbce59b0
Jaycie <10757533@my.uvu.edu>
2019-10-25 13:49:25 -0600

    Added/Practiced weapon collection in unity Only did a for each right now

---
+++ BT21 Game/Assets/Scripts/Collections/WeaponCollection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+[CreateAssetMenu]
+public class WeaponCollection : ScriptableObject
+{
+    public List<PlayerWeaponData> collectionList;
+
+    public void AddData(PlayerWeaponData obj)
+    {
+        foreach (var weaponCollection in collectionList)
+        {
+            Debug.Log(weaponCollection);
+        }
+    }
+    
+
+}
---
+++ BT21 Game/Assets/Scripts/Data Scripts/PlayerWeaponData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class PlayerWeaponData : ScriptableObject
+{
+	public Sprite weaponDesign;
+	public Color weaponColor = Color.red;
+	public int powerLevel = 10;
+	public GameObject prefab;
+	public string weaponDescription;
+
+}
