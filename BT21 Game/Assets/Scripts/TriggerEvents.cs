5a1c9786411e87bd2a4889bf6bc53d86c79a1a27
Jaycie <10757533@my.uvu.edu>
2019-10-28 20:33:09 -0600

    Fixed jump (Now double jump doesnt work)
    also added collection but I'm getting an error so i commented it out.

---
+++ BT21 Game/Assets/Scripts/TriggerEvents.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class TriggerEvents : MonoBehaviour
+{
+	public UnityEvent triggerEvent;
+
+	private void Awake()
+	{
+		GetComponent<Collider>().isTrigger = true;
+	}
+
+	//private void OnTriggerEnter(Collider other)
+	//{
+	//	triggerEvent.Invoke();
+	//}
+}
+	
+	
