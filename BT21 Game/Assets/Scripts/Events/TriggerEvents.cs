0984e45e3c6a426807e38b6044bdde87fe159dec
Jaycie <10757533@my.uvu.edu>
2019-10-30 14:21:33 -0600

    Trying to add Weapon Switch i think I'm doing it wrong So I will continue after work

---
+++ BT21 Game/Assets/Scripts/Events/TriggerEvents.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class TriggerEvents : MonoBehaviour
+{
+	public UnityEvent triggerEvent;
+	public UnityEvent triggerEnterEvent;
+	private void Awake()
+	{
+		GetComponent<Collider>().isTrigger = true;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		triggerEnterEvent.Invoke();
+	}
+}
+	
+	
---
+++ BT21 Game/Assets/Scripts/WeaponsHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class WeaponsHandler : MonoBehaviour
+{
+
+	public Debugger weaponObj;
+
+	public void SwitchWeapon(Debugger newWeaponObj)
+	{
+		weaponObj = newWeaponObj;
+	}
+
+	//private void Update()
+	//{
+	//	weaponObj.
+	//}
+}
+
+//I'm really confused. 
