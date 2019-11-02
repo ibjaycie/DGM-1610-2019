e575663146e10b33b5b7753c7163e9d110078695
Jaycie <10757533@my.uvu.edu>
2019-11-01 22:35:37 -0600

    Trying to require a NavMeshObstacle but not sure where I should put it to make it effective.

--- BT21 Game/Assets/Scripts/Data Scripts/Bt21PlayerData.cs
+++ BT21 Game/Assets/Scripts/Data Scripts/Bt21PlayerData.cs
@@ -1,7 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 [CreateAssetMenu (menuName = "Character")]
+//[RequireComponent(typeof(NavMeshObstacle))] Trying to make it so I can require the 
+//the nav mesh obstacle so i don't hae to apply it to every prefab. 
 public class Bt21PlayerData : ScriptableObject
 {
 	public string friendName;
