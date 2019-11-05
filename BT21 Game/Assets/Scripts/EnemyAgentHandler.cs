956487a0b4ff38f0fedff2e2b50b1523b9de1745
Jaycie <10757533@my.uvu.edu>
2019-11-05 15:21:16 -0700

    reviewed AI In class Trying to make the AI follow my prefab.

--- BT21 Game/Assets/Scripts/EnemyAgentHandler.cs
+++ BT21 Game/Assets/Scripts/EnemyAgentHandler.cs
@@ -8,33 +8,17 @@ using UnityEngine.AI;
 public class EnemyAgentHandler : MonoBehaviour
 {
 	private NavMeshAgent agent;
-	private Transform enemyCurrentDestination;
-	public Transform[] destinationObject; //If I make this an array i should be able to add more players. But it isn't working. 
-	private GameObject startObject;
+	public Vector3 location;
+	public GameObject characterPrefab;
+
 
 	private void Start()
 	{
-		startObject = new GameObject();
-		startObject.transform.position = transform.position;
-		enemyCurrentDestination = transform;
-		agent = GetComponent<NavMeshAgent>();	
+		agent = GetComponent<NavMeshAgent>(); 
 	}
 
-	private void OnTriggerEnter(Collider other)
+	private void Update()
 	{
-		enemyCurrentDestination = destinationObject;
+		agent.destination = location;
 	}
-
-	private void OnTriggerExit(Collider other)
-	{
-		enemyCurrentDestination = startObject.transform;
-	}
-
-	//private void Update()
-	//{
-	//	agent.destination = enemyCurrentDestination.position;
-	//}
-	//currently getting an error that says "Set Destination" can only be called on an
-	//object that has an active navmesh. Idk what that means because I have
-	//a nav mesh attached to the object. 
 }
