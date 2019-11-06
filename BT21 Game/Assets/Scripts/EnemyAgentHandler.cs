1aace331718fe171b052b8a5a48f44edc0e1a4b4
Jaycie <10757533@my.uvu.edu>
2019-11-06 13:53:04 -0700

    Worked on AI again and got it to work I needed to make the enemy boxcollider bigger as well as make the Location = transform //in order for it to follow correctly.

--- BT21 Game/Assets/Scripts/EnemyAgentHandler.cs
+++ BT21 Game/Assets/Scripts/EnemyAgentHandler.cs
@@ -8,17 +8,27 @@ using UnityEngine.AI;
 public class EnemyAgentHandler : MonoBehaviour
 {
 	private NavMeshAgent agent;
-	public Vector3 location;
-	public GameObject characterPrefab;
+	private Transform location;
+	public Transform playerDestination;
 
 
 	private void Start()
 	{
+		location = transform;
 		agent = GetComponent<NavMeshAgent>(); 
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		location = playerDestination;
 	}
 
 	private void Update()
 	{
-		agent.destination = location;
+		agent.destination = location.position;
 	}
 }
+
+
+//IT WORKED! I needed to make the enemy boxcollider bigger as well as make the Location = transform
+//in order for it to follow correctly. 
