625ecb71fc119640a93e8b6e7ac6981f5033550b
Jaycie <10757533@my.uvu.edu>
2019-11-08 22:25:32 -0700

    Not sure how to add a min max to a list

---
+++ BT21 Game/Assets/Scripts/PlayerMaxWeapons.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMaxWeapons : MonoBehaviour
+{
+	public int weaponMax = 2;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		throw new NotImplementedException();
+	}
+}
+//I actually don't think I need this script. 
