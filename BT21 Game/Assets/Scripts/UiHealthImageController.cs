577e8efc49a41376be6e020484182ce690e18e84
Jaycie <10757533@my.uvu.edu>
2019-11-09 19:49:27 -0700

    Added Health Bar
    Added Health Bar
    NOw i need to add scripts to the enemy to reduce and add health

---
+++ BT21 Game/Assets/Scripts/UiHealthImageController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+[RequireComponent(typeof(Image))]
+public class UiHealthImageController : MonoBehaviour
+{
+	private Image healthBar;
+
+	private void Awake()
+	{
+		healthBar = GetComponent<Image>();
+	}
+
+	public void UpdateImage(HealthData data)
+	{
+		healthBar.fillAmount = data.totalHealth;
+	}
+}
