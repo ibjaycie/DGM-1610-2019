7312cebb80c2bd1559be2f5800abb9612b27be86
Jaycie <10757533@my.uvu.edu>
2019-11-09 19:49:16 -0700

    Added Health Bar NOw i need to add scripts to the enemy to reduce and add health

---
+++ BT21 Game/Assets/Scripts/Data Scripts/HealthData.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class HealthData : ScriptableObject
+{
+	public float totalHealth = 1f;
+	public float minHealth = 0f;
+	public float maxHealth = 1f;
+
+	public void UpdateHealth(float amount)
+	{
+		totalHealth += amount;
+	}
+
+	public void UpdateHealthRange(float amount)
+	{
+		if (totalHealth <= maxHealth)
+		{
+			UpdateHealth(amount);
+		}
+		else
+		{
+			totalHealth = maxHealth;
+		}
+
+		if (totalHealth >= minHealth)
+		{
+			UpdateHealth(amount);
+		}
+		else
+		{
+			totalHealth = minHealth;
+		}
+	}
+
+}
