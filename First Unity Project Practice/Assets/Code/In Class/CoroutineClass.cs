8bd86d05862f5f62122559fe9606731e60909b9e
Jaycie <10757533@my.uvu.edu>
2019-11-14 15:44:07 -0700

    Reviewed Coroutines in class I added the particles emit my self ;)

---
+++ First Unity Project Practice/Assets/Code/In Class/CoroutineClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineClass : MonoBehaviour
+{
+	public bool canRun = true;
+	public IntDataObject index; //this way you can effect any float data you've created. 
+	public float seconds = 1f;
+
+	private WaitForSeconds wfsObj;
+
+	public void Run()
+	{
+		wfsObj = new WaitForSeconds(seconds); //created 1 time and used many times. 
+		StartCoroutine(OnRun());
+	}
+	IEnumerator OnRun()
+	{
+		while (index.value > 0)
+		{
+			index.value--;
+			Debug.Log(index);
+			yield return wfsObj;
+			
+		}
+	}
+}
+
+
---
+++ First Unity Project Practice/Assets/CoroutinePractice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CoroutinePractice : MonoBehaviour
+{
+    public ParticleSystem explode;
+    public UnityEvent ParticleExplosion;
+    
+    
+    
+    IEnumerator OnTriggerEnter(Collider other)
+    {
+        while (explode)
+        {
+            yield return new WaitForSeconds(3f);
+            Debug.Log("boom");
+            explode.Emit(100);
+        }
+        
+    }
+}
