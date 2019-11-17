fb3e0640e7716ef96f3d21d979574f5212bfd06e
Jaycie <10757533@my.uvu.edu>
2019-11-16 22:53:10 -0700

    Worked on Countdown Coroutine
    I can't figure out how to make it pause the MOVEMENT as well as countdown.

---
+++ BT21 Game/Assets/Scripts/MovementDelayCoroutine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MovementDelayCoroutine : MonoBehaviour
+{
+	public UnityEvent movementDelay;
+	private WaitForSeconds wfsobj = new WaitForSeconds(3f);
+	public CharacterMovement script;
+	private void Start()
+	{
+		StartCoroutine(PauseMovement());
+	}
+
+	IEnumerator PauseMovement()
+	{
+		while (script)
+		{
+			yield return wfsobj;
+			Debug.Log("Get Ready");
+		}
+		yield return wfsobj;
+		Debug.Log("You Can Move Now");
+	}
+}
