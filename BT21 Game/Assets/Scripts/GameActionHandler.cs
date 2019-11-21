d9bff92aea3dafae8de7b020c1905ce09afb6d5f
Jaycie <10757533@my.uvu.edu>
2019-11-21 16:39:34 -0700

    Worked on Animator and Polishing Countdown Script
    Some stuff not working, figuring it out.

---
+++ BT21 Game/Assets/Scripts/GameActionHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameActionHandler : MonoBehaviour
+{
+	public GameAction gameActionObj;
+	public UnityEvent handlerEvent;
+	private void Start()
+	{
+		gameActionObj.action = RaiseHandler;
+	}
+
+	public void RaiseHandler()
+	{
+		handlerEvent.Invoke();
+	}
+}
---
+++ BT21 Game/Assets/StatesBehavoir.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class StatesBehavoir : StateMachineBehaviour
+{
+
+	public UnityEvent StartEnterEvent, StateUpdateEvent, StateExitEvent; 	 //an event that runs at the start of any state. 
+	
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) 
+	{
+     	StartEnterEvent.Invoke();
+	}
+
+	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		StateUpdateEvent.Invoke();
+	}
+
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) 
+	{
+		StateExitEvent.Invoke();
+	}
+
+	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+}
