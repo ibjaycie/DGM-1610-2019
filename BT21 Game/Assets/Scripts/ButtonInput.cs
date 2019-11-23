fd6c498fa80dd90a38765fd3d45322b0c3fe7737
Jaycie <10757533@my.uvu.edu>
2019-11-22 21:57:59 -0700

    Added Button Event Script
    Script I missed from class

---
+++ BT21 Game/Assets/Scripts/ButtonInput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class ButtonInput : MonoBehaviour
+{
+	public UnityEvent inputEvent;
+	public string inputWord;
+
+	private void Update()
+	{
+		if (Input.GetButtonDown(inputWord))
+		{
+			inputEvent.Invoke();
+		}
+	}
+}
