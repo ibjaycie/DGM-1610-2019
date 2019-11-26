2ff1abd56221eef1e41310188088004a34b4241e
Jaycie <10757533@my.uvu.edu>
2019-11-26 14:24:17 -0700

    Added Coins

---
+++ BT21 Game/Assets/Scripts/TransformCoin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformCoin : MonoBehaviour
+{
+    public int rotateSpeed;
+
+    private void Update()
+    {
+        transform.Rotate(0, rotateSpeed, 0, Space.World);
+    }
+}
+//that was way easier than I thought it would be. 
