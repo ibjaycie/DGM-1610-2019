7346c6f07138226a52e4018b29188c211cd31500
Jaycie <10757533@my.uvu.edu>
2019-11-30 17:51:24 -0700

    Working on Speed powerup

---
+++ BT21 Game/Assets/Scripts/SpeedPowerUpEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedPowerUpEvent : MonoBehaviour
+{
+	private float powerSpeed = 10f;
+	public GameObject[] playerPrefab;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		StartCoroutine(SpeedPowerUp());
+	}
+
+	IEnumerator SpeedPowerUp()
+	{
+		//playerPrefab. = powerSpeed;
+		yield return new WaitForSeconds(3f);
+		Debug.Log("Times Up for Power Up Speed");
+	}
+}
