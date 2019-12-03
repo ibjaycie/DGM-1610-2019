c3b491b8306279a43e9f1d3c30d74a0699545cba
Jaycie <10757533@my.uvu.edu>
2019-12-03 15:49:05 -0700

    Added destory and flip script in class

---
+++ BT21 Game/Assets/ApplyForce.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class ApplyForce : MonoBehaviour
+{
+	private Rigidbody rb;
+	public Vector3Data forceDirection;
+	public float force = 3f;
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		rb.AddForce(forceDirection.value*force);
+	}
+}
---
+++ BT21 Game/Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour
+{
+	public float delay = 3f;
+	private void Start()
+	{
+		Destroy(gameObject, delay);
+	}
+
+	public void DestroyOnImpact()
+	{
+		Destroy(gameObject);
+	}
+}
