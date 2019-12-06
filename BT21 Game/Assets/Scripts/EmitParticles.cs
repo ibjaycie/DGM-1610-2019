88722cd8f6473ba87166d9147e9075e291803db4
Jaycie <10757533@my.uvu.edu>
2019-12-06 15:03:34 -0700

    Added Particle System with Artwork
    interacts with the ground

---
+++ BT21 Game/Assets/Scripts/EmitParticles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmitParticles : MonoBehaviour
+{
+	public ParticleSystem particles;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		particles.Emit(50);
+	}
+}
