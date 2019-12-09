5a522f32078d9750461e09a1713c691f97c6e851
Jaycie <10757533@my.uvu.edu>
2019-12-09 10:54:54 -0700

    Worked on Swtich and State Machine Had help fromt tutorial with State Machine

---
+++ BT21 Game/Assets/Scripts/StaeMachineAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaeMachineAnimation : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) ||Input.GetKey(KeyCode.RightArrow))
+        {
+            anim.SetBool("IsWalking", true);
+            Debug.Log("You're Walking");
+        }
+        else
+        {
+            anim.SetBool("IsWalking", false);
+            Debug.Log("You're Idle");
+        }
+    }
+}
