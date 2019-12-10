6f261f8319677722d0bdc3d35080cbdcab29a998
Jaycie <10757533@my.uvu.edu>
2019-12-10 08:52:13 -0700

    Created Weapon switch With help of Brackey's
    I realized what I was trying to do with On trigger Enter wasn't working at all so I needed help. 
    It WORKS but, the sword shoots as well

---
+++ BT21 Game/Assets/Scripts/StateMachineAnimation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachineAnimation : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) ||Input.GetKey(KeyCode.RightArrow))
+        {
+            anim.SetBool("IsWalking", true);
+            Debug.Log("You're Walking");
+        }
+        else
+        {
+            anim.SetBool("IsWalking", false);
+            Debug.Log("You're Idle");
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            anim.SetBool("IsJumping", true);
+            Debug.Log("You're Walking");
+        }
+    }
+}
