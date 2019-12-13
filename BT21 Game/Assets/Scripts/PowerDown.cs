08fc42b2b3fb4b34a9012920dc0cb7bf81e7644e
Jaycie <10757533@my.uvu.edu>
2019-12-12 18:53:12 -0700

    Added Power Down Script
    with Coroutine

---
+++ BT21 Game/Assets/Scripts/PowerDown.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerDown : MonoBehaviour 
+{
+	private WaitForSeconds  wait = new WaitForSeconds(4);
+	private void OnTriggerEnter(Collider other)
+	{
+		StartCoroutine(pickupPowerDown(other));
+	}
+
+	IEnumerator pickupPowerDown(Collider player)
+	{
+		CharacterMovement powerDownSpeed = player.GetComponent<CharacterMovement>();
+        
+		powerDownSpeed.speed = 3f;
+        
+		GetComponent<MeshRenderer>().enabled = false;
+		GetComponent<Collider>().enabled = false;
+        
+		yield return wait;
+        
+		powerDownSpeed.speed = 10f;
+        
+		Destroy(gameObject);
+	}
+	
+}
