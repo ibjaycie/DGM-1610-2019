7c4840be81c7b693ff5b7e457b2b0142dfde7d83
Jaycie <10757533@my.uvu.edu>
2019-12-16 22:21:37 -0700

    Added Character Movement for Vampire Player
    Changed some things

---
+++ Vampire Game/Assets/Scripts/VampireMovement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+public class VampireMovement : MonoBehaviour
+{
+	public CharacterController controller;
+	public float speed = 1f;
+	public float jumpSpeed = 15f;
+	public float gravity = 50f;
+
+	private int jumpCount;
+	public int jumpCountMax = 2;
+	private Vector3 position;
+	public Vector3 locationData;
+	private void Start()
+	{
+		controller = GetComponent<CharacterController>();
+	}
+	
+	private void Update()
+	{
+		locationData = transform.position;
+		position.x = speed * Input.GetAxis("Horizontal");
+		controller.Move(position * Time.deltaTime); 
+		
+		if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
+		{
+			position.y = jumpSpeed;
+			jumpCount++;
+		}
+		if (controller.isGrounded)
+		{
+			
+			jumpCount = 0;
+		}
+
+		
+		position.y -= gravity * Time.deltaTime;
+	}
+}
