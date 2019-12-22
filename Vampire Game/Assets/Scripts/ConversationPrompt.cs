f19891e15c0af51f5a9a67b5a9e61906ec364b2c
Jaycie <10757533@my.uvu.edu>
2019-12-21 21:36:43 -0700

    Working on Covnersation Coroutine

--- Vampire Game/Assets/Scripts/ConversationPrompt.cs
+++ Vampire Game/Assets/Scripts/ConversationPrompt.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class ConversationPrompt : MonoBehaviour
 {
     public GameObject canvas;
+    
 
     private void Start()
     {
@@ -15,10 +16,17 @@ public class ConversationPrompt : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         canvas.SetActive(true);
+        StartCoroutine(ConversationBreak());
     }
 
+    IEnumerator ConversationBreak()
+    {
+        yield return new WaitForSeconds(3);
+        Debug.Log("Next Response");
+    }
     private void OnTriggerExit(Collider other)
     {
         canvas.SetActive(false);
     }
+    
 }
