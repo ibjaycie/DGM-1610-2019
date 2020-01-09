0fd6c815ebef29a7a9308913886af29984fc2b4b
Jaycie <10757533@my.uvu.edu>
2020-01-08 18:16:14 -0700

    Added Fill amount script to help with Coroutine

---
+++ Vampire Game/Assets/Scripts/UIHealthImageControllerBloodSupply.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class UIHealthImageControllerBloodSupply : MonoBehaviour
+{
+	private Image healthBar;
+
+	private void Awake()
+	{
+		healthBar = GetComponent<Image>();
+	}
+
+	public void UpdateImage(PlayerHealthData data)
+	{
+		healthBar.fillAmount = data.totalHealth;
+	}
+}
