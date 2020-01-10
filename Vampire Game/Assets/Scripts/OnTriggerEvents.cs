ace882e0ef2cda9237d6edc5756524e5d71d95d5
Jaycie <10757533@my.uvu.edu>
2020-01-09 18:49:54 -0700

    Attempt at making the health bar depelete on start over time

---
+++ Vampire Game/Assets/Scripts/OnTriggerEvents.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class OnTriggerEvents : MonoBehaviour
+{
+	public UnityEvent onTriggerEvent;
+
+	private void Awake()
+	{
+		GetComponent<Collider>().isTrigger = true;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		onTriggerEvent.Invoke();
+	}
+}
