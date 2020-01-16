e13c7c8f9d6d0d08e3c3e01a9bb7f48cd8446b33
Jaycie <10757533@my.uvu.edu>
2020-01-15 20:19:32 -0700

    YES I GOT IT TO WORK SO
    SO basically the player has 60 seconds to drink someones blood before they die. 
    had help from Unity BUT WOOT WOOT!!

---
+++ Vampire Game/Assets/Scripts/BloodBar.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class BloodBar : MonoBehaviour
+{
+	public GameObject healthImageCanvas;
+	public Slider slider;
+	private Image healthBar;
+
+	private void Start()
+	{
+		healthImageCanvas.SetActive(true);
+		slider.value = 1f;
+		StartCoroutine(BloodBarCountdown());
+	}
+
+	IEnumerator BloodBarCountdown()
+	{
+		//below help from unity
+		if (slider != null)
+		{
+			float timeSlice = (slider.value / 60);
+			while (slider.value >= 0)
+			{
+				slider.value -= timeSlice;
+				yield return new WaitForSeconds(1);
+				if (slider.value <= 0)
+					break;
+			}
+		}
+		yield return null;
+		//while (true)
+		//{
+		//	Debug.Log ("OnCoroutine: "+(int)Time.time);
+		//	yield return new WaitForSeconds(1f);
+		//	slider.value = 
+		//}
+	}
+}
