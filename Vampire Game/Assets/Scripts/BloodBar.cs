c331b2c7ed5896cc44b0fe96c8a8eec0579aa3ac
Jaycie <10757533@my.uvu.edu>
2020-01-24 21:30:20 -0700

    Figured out how to stop coroutine and add health however, it won't restart coroutne in exit

--- Vampire Game/Assets/Scripts/BloodBar.cs
+++ Vampire Game/Assets/Scripts/BloodBar.cs
@@ -14,6 +14,7 @@ public class BloodBar : MonoBehaviour
 		healthImageCanvas.SetActive(true);
 		slider.value = 1f;
 		StartCoroutine(BloodBarCountdown());
+		
 	}
 
 	IEnumerator BloodBarCountdown()
@@ -31,40 +32,5 @@ public class BloodBar : MonoBehaviour
 			}
 		}
 		yield return null;
-		//while (true)
-		//{
-		//	Debug.Log ("OnCoroutine: "+(int)Time.time);
-		//	yield return new WaitForSeconds(1f);
-		//	slider.value = 
-		//}
-	}
-
-	public void OnTriggerEnter(Collider target)
-	{
-		StopCoroutine(BloodBarCountdown());
-		StartCoroutine(ReverseBloodBarCountdown());
-		
-	}
-
-	public void OnTriggerExit(Collider other)
-	{
-		StopCoroutine(ReverseBloodBarCountdown());
-		StartCoroutine(BloodBarCountdown());
-	}
-
-	public IEnumerator ReverseBloodBarCountdown()
-	{
-		Debug.Log("Filling up on blood");
-		if (slider != null)
-		{
-			float timeSlice = (slider.value / 30);
-			while (slider.value >= 0)
-			{
-				slider.value += timeSlice;
-				yield return new WaitForSeconds(1);
-				if (slider.value <= 0)
-					break;
-			}
-		}
 	}
 }
