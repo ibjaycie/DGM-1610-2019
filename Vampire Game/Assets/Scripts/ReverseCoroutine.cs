0c75583f628862a2b0eee564897d1d293adbb789
Jaycie <10757533@my.uvu.edu>
2020-01-24 21:30:43 -0700

    Figured out how to stop coroutine and add health
    however, it won't restart coroutne in exit

---
+++ Vampire Game/Assets/Scripts/ReverseCoroutine.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReverseCoroutine : MonoBehaviour 
+{
+	public GameObject healthImageCanvas;
+	public Slider slider;
+	private Image healthBar;
+	
+	public void OnTriggerEnter(Collider target)
+	{
+		StartCoroutine(ReverseBloodBarCountdown());
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		StopCoroutine(ReverseBloodBarCountdown());
+	}
+
+	public IEnumerator ReverseBloodBarCountdown()
+	{
+		Debug.Log("Filling up on blood");
+		if (slider != null)
+		{
+			float timeSlice = (slider.value / 30);
+			while (slider.value >= 0)
+			{
+				slider.value += timeSlice;
+				yield return new WaitForSeconds(1);
+				if (slider.value <= 0)
+					break;
+			}
+		}
+
+		yield return null;
+	}
+	
+}
